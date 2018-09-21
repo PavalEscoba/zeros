9c3e7cb7d7602db72b9986296947674c5c3cc258
Paval Miatlitski <paval.escoba@gmail.com>
2018-09-21 23:47:21 +0300

    2 solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  //my own solution
+  let result = 0;
+  let pow = 1;
+  while (5 ** pow < number){
+    result += Math.floor(number/(5**pow));
+    pow +=1;
+  }
+  return result;
+/*   let zeros = 0; // элегантное решение отсюда https://site.ada.edu.az/~medv/acm/Docs%20e-olimp/Volume%202/123.htm
+  while(number > 0){
+    number = Math.trunc(number / 5);
+    zeros += number;
+  }
+  return zeros; */
 }
